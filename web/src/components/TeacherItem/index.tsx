6de65886b86db12287f5d7470babcb304ae99461
nascimentorapha <nascimento.rapha@hotmail.com>
2020-08-26 16:49:52 -0300

    changed teacher infos from hardcoded to interface with propertys

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,37 +1,45 @@
 import React from 'react';
 
-import './styles.css';
-
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
+import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
     return(
         <article className="teacher-item">
             <header>
-                <img src="https://avatars0.githubusercontent.com/u/49926686?s=460&u=f406cea8de69ca1761fabe4cd5647f61b212e5f5&v=4" alt="Professor"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Raphael Nascimento</strong>
-                    <span>Física</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p> 
-                Entusiasta por Deus e tudo que envolve Ele
-                <br/>
-                <br/>
-                Buscando viver diariamente uma vida de acordo com as Escrituras sagradas e comunhão com o Espírito Santo
-                </p>
+                <p>{teacher.bio}</p>
 
                 <footer>
                     <p>
                         Preço/hora
-                        <strong>R$65,00</strong>
+                        <strong>R$ {teacher.cost}</strong>
                     </p>
-                    <button type="button">
+                    <a href={`https://wa.me/${teacher.whatsapp}`}>
                         <img src={whatsappIcon} alt="Whatsapp"/>
                         Entrar em contato
-                    </button>
+                    </a>
                 </footer>
         </article>
     )
